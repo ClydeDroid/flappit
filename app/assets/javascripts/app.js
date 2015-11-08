e3f6c39028c0cb51ddd6ebbff484620b92cf0c24
ClydeDroid <clydedroid@gmail.com>
2015-11-08 14:56:10 -0500

    Setting up the database

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,16 +1,16 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var app = angular.module('flapperNews', ['ui.router', 'templates']);
 
 // Config
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: '/home.html',
+            templateUrl: 'home/_home.html',
             controller: 'MainCtrl'
         })
         .state('posts', {
             url: '/posts/{id}',
-            templateUrl: '/posts.html',
+            templateUrl: 'posts/_posts.html',
             controller: 'PostsCtrl'
         });
     $urlRouterProvider.otherwise('home');
