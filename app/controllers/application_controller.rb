e37d73ec2509101dfd5e15d8e0846738f03bcaf2
ClydeDroid <clydedroid@gmail.com>
2015-11-13 15:56:30 -0500

    Trying to fix csrf stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_filter :set_csrf_cookie_for_ng
 
   def angular
     render 'layouts/application'
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions'}
   root 'pages#angular'
 
   resources :posts, only: [:create, :index, :show] do
